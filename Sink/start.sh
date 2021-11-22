1fc0fc4cda99199fa8e2e905bf95accf57cba3a8
Academia <academia@academia.kubernetes001>
2021-11-22 08:58:10 +0000

    some changes

--- Sink/start.sh
+++ Sink/start.sh
@@ -2,4 +2,4 @@
 
 while [ "$(curl -s -o /dev/null -w ''%{http_code}'' http://pulsar-mini-proxy:80/status.html)" != "200" ]; do sleep 5; done
 
-java -jar ./target/PostgresSink-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -javaagent:./jmx_prometheus_javaagent-0.16.1.jar=9000:config.yaml -jar ./target/PostgresSink-1.0-SNAPSHOT-jar-with-dependencies.jar
